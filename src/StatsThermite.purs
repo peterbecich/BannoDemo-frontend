fe22af982bea9a2f3c8c28959e7ca74920d5988d
Peter Becich <peterbecich@gmail.com>
2017-12-27 13:31:11 -0800

    attempting to start timer in Aff upon component loading

--- src/StatsThermite.purs
+++ src/StatsThermite.purs
@@ -7,6 +7,9 @@ import Control.Monad.Aff
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
 import Control.Monad.Trans.Class (lift)
+
+import Control.Monad.Eff.Console (CONSOLE, log)
+
 import Data.Newtype (wrap)
 
 import Data.Either
@@ -22,8 +25,11 @@ import RetrieveTwitterStats (retrieveTwitterStats)
 import Types.TwitterStats
 
 statsThermite :: T.Spec _ _ _ _
-statsThermite = T.simpleSpec T.defaultPerformAction statsRender
+statsThermite = T.simpleSpec statsAction statsRender
   where
+    statsAction :: T.PerformAction _ _ _ _
+    statsAction _ _ _ = tickLoop
+    
     statsRender :: T.Render TwitterStats _ _
     -- statsRender _ _ _ _ = [ R.p'  [ R.text "Tweet count: " ] ]
     statsRender _ _ (TwitterStats {tweetCount, emojiTweetCount, urlTweetCount, picTweetCount, hashtagTweetCount}) _ =
@@ -33,14 +39,15 @@ statsThermite = T.simpleSpec T.defaultPerformAction statsRender
 
 tickLoop :: T.CoTransformer _ _ _ _
 tickLoop = do
+  _ <- liftEff $ log $ show "tick loop"
   _ <- lift $ delay (wrap (1000.0))
-  eitherStats <- liftEff $ launchAff $ retrieveTwitterStats ""
-  -- case eitherStats of
-  --   (Left _) -> pure unit
-  --   (Right stats') -> pure unit
-  --     -- do
-  --     --   T.modifyState(\_ -> stats')
-  T.modifyState(id)
+  _ <- liftEff $ launchAff $ do
+       eitherStats <- retrieveTwitterStats ""
+       liftEff $ log $ show $ eitherStats
+       -- case eitherStats of
+       --   (Left _) -> T.modifyState(id)
+       --   (Right stats') -> T.modifyState(\_ -> stats')
+  tickLoop
 
 errorThermite :: T.Spec _ _ _ _
 errorThermite = T.simpleSpec T.defaultPerformAction errorRender
