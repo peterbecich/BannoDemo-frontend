4256a3fecc0907c78da0156aa321193591b8f0e4
Peter Becich <peterbecich@gmail.com>
2017-12-27 11:52:57 -0800

    timed reloading of stats in progress

--- src/StatsThermite.purs
+++ src/StatsThermite.purs
@@ -2,12 +2,22 @@ module StatsThermite where
 
 import Prelude
 
+import Control.Monad
+import Control.Monad.Aff
+import Control.Monad.Eff
+import Control.Monad.Eff.Class
+import Control.Monad.Trans.Class (lift)
+import Data.Newtype (wrap)
+
+import Data.Either
+
 import Thermite as T
 
 import React as R
 import React.DOM as R
 import React.DOM.Props as RP
 
+import RetrieveTwitterStats (retrieveTwitterStats)
 
 import Types.TwitterStats
 
@@ -21,6 +31,16 @@ statsThermite = T.simpleSpec T.defaultPerformAction statsRender
       , R.p'  [ R.text "Emoji tweet count: ", R.text (show emojiTweetCount) ]
       ]
 
+tickLoop :: T.CoTransformer _ _ _ _
+tickLoop = do
+  _ <- lift $ delay (wrap (1000.0))
+  eitherStats <- liftEff $ launchAff $ retrieveTwitterStats ""
+  -- case eitherStats of
+  --   (Left _) -> pure unit
+  --   (Right stats') -> pure unit
+  --     -- do
+  --     --   T.modifyState(\_ -> stats')
+  T.modifyState(id)
 
 errorThermite :: T.Spec _ _ _ _
 errorThermite = T.simpleSpec T.defaultPerformAction errorRender
